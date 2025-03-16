93c56469be208fec271a0cf7213e35203517c485
Ondrej Faiman <ondraFaiman@atlas.cz>
2025-03-16 13:36:18 +0100

    Updated test main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-#include "Hamiltonian_functions.h"
+#include "Hamiltonian_functions_real.h"
+#include "BandStructure.h"
 #include <complex>
 #include <memory>
 #include<utility>
 using namespace std;
 int main(){
  	hamiltonian Ham("INPUT.txt");
-	Ham.diagonal_at_k_point(.25, .5, 1.0);
- 	hamiltonian Ham_2(move(Ham));
-	Ham_2.diagonal_at_k_point(.25, .5, 1.0);
-	cout << Ham.get_value(0);
+	band_structure Band(Ham);
+	Band.do_plot();
 }
 

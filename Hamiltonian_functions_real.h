c0c8fb839c0be936abbb2195edaa674cdbd5d368
Ondrej Faiman <ondraFaiman@atlas.cz>
2025-03-16 13:31:37 +0100

    Hamiltonian is now double the original size in order to make it real and summetrical

---
+++ Hamiltonian_functions_real.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <complex>
+#include <iostream>
+#include <string>
+#include <set>
+#include <memory>
+#include <vector>
+#include <map>
+using c_d = std::complex<double>;
+
+class hamiltonian{
+public:
+	hamiltonian();
+	hamiltonian(std::string filename_1);
+	~hamiltonian();
+	hamiltonian(hamiltonian &&other);
+	void diagonal_at_k_point(double kx, double ky, double kz);
+	void get_vector(int n, std::unique_ptr<double[]>& array);
+       	double get_value(int n);
+	std::string get_filename();
+	double get_lattice_const();
+	int get_size();
+	void print_ham();
+	static std::map<std::string, double> parameters;
+private:
+	static std::set<std::string> expected_parameters;
+	static std::set<std::string> extra_parameters;
+	void assemble(double kx, double ky, double kz);
+	void to_matrix(c_d (&input)[9]);
+	void copy_other_half();
+	void copy_to_bottom();
+	void read_values(std::string filename_1);
+	void tolower_string(std::string &input);
+	double string_to_double(std::string val, std::string keyword);
+	void make_tracer(std::map<std::string, bool> &input);
+	void is_all_loaded(std::map<std::string, bool> &input);
+	int size;
+	static std::string filename; 
+	std::unique_ptr<double[]> values;
+	std::unique_ptr<double[]> eigen_vals;
+
+};
